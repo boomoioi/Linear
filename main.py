9ed41cb57ea8eb800ff3deaebc2342272a565bc2
Nanthakorn <64312642+boomoioi@users.noreply.github.com>
2023-10-25 21:51:52 +0700

    Final

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ def predict(company, count, axis):
     df = pdr.get_data_yahoo(company, start=datetime.now() - timedelta(days=120), end=datetime.now())
     df = df.filter(["Close"])
     dataset = df.values
+    
     from sklearn.preprocessing import MinMaxScaler
-
     scaler = MinMaxScaler(feature_range=(0,1))
     scaled_data = scaler.fit_transform(dataset)
     test_data = scaled_data[0: , :]
@@ -28,11 +28,14 @@ def predict(company, count, axis):
 
     x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1 ))
     model = tf.keras.saving.load_model("my_model.keras")
+    
     predictions = model.predict(x_test)
     predictions = scaler.inverse_transform(predictions)
+    
     print()
     print("--------------------------------------------------------------------------")
     print()
+    
     rmse = np.sqrt(np.mean(((predictions - y_test) ** 2)))
     print(rmse)
     valid = df[60:]
@@ -46,16 +49,13 @@ def predict(company, count, axis):
 def correlation(com1, com2):
     stocks = [com1, com2]
     data = yf.download(stocks, start=datetime.now() - timedelta(days=10000), end=datetime.now())['Adj Close']
-
     corr_df = data.corr(method='pearson')
     corr_df.head().reset_index()
     print(corr_df)
 
-
-
-figure, axis = plt.subplots(2, 1) 
 con = "Y"
-while con != "N":
+while con == "Y":
+    figure, axis = plt.subplots(2, 1) 
     com1 = input("Enter first company : ")
     com2 = input("Enter second company : ")
     predict(com1, 0, axis)
